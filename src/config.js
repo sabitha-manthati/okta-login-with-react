6a7a4b1b7beb6d7adc30d658de4f6942989d1757
sabitha manthati <65341379+sabitha-manthati@users.noreply.github.com>
2021-09-29 17:48:26 +0530

    Update config.js

--- src/config.js
+++ src/config.js
@@ -3,13 +3,13 @@ const oktaAuthConfig = {
     // instead of the Authorization Code with Proof of Code Key Exchange (PKCE)
     // you will need to add `pkce: false`
     issuer: 'https://dev-79516127.okta.com/oauth2/default',
-    clientId: '0oa213zzxx63Rhgxr5d7',
+    clientId: '0oa21pk7xs8LEThHE5d7',
     redirectUri: window.location.origin + '/login/callback',
   };
   
   const oktaSignInConfig = {
     baseUrl: 'https://okta-login-form.herokuapp.com',
-    clientId: '0oa213zzxx63Rhgxr5d7',
+    clientId: '0oa21pk7xs8LEThHE5d7',
     redirectUri: window.location.origin + '/login/callback',
     // authParams: {
     //   // If your app is configured to use the Implicit flow
